1283179401228eb996a25921be11437a55c0ae97
= <tboggs300@gmail.com>
2022-12-23 23:12:41 -0500

    Placeholder fix in nodejs basic-web-server

--- nodejs/basic-web-server/index.js
+++ nodejs/basic-web-server/index.js
@@ -3,7 +3,7 @@ const server = require('fastify')();
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || '4000';
 
-console.log(`worker pid: process.pid`);
+console.log(`worker pid: ${process.pid}`);
 
 server.get('/hello', async (req, res) => {
     console.log(`worker request pid: ${process.pid}`);
